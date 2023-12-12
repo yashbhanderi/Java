bdf4f93de562600de1fd6bae2d20bb0d40112ec9
Yash Bhanderi <yashbhanderi1818@gmail.com>
2023-12-13 00:51:42 +0530

    Dec 13, 2023

---
+++ src/arrays/IsArraySorted.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class IsArraySorted {
+
+    public static void main(String[] args) {
+
+        Integer[] arr = {1, 3, 4, 8, 9};
+
+        System.out.println(isArraySorted(arr));
+
+    }
+
+    private static boolean isArraySorted(Integer[] arr) {
+
+        for(int i=0; i<arr.length-1; i++) {
+            if(arr[i] > arr[i+1]) return false;
+        }
+
+        return true;
+    }
+}
