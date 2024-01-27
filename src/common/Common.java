4a64e40ada9063a9c7685da41661b98ea09cd88c
Yash Bhanderi <yash.bhanderi@marutitech.com>
2024-01-27 15:04:13 +0530

    Jan 27, 2023

---
+++ src/common/Common.java
@@ -0,0 +1,22 @@
+package common;
+
+public class Common {
+
+    public static void reverseArray(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
