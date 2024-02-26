02995a523188284019518f044cf681dd3b8f57b9
Yash Bhanderi <146318098+yash-bhanderi@users.noreply.github.com>
2024-02-27 00:50:06 +0530

    Feb 27, 2023

--- src/binary_search/3_Upper_Bound.java
+++ src/binary_search/3_Upper_Bound.java
@@ -2,13 +2,42 @@ package binary_search;
 
 import common.Common;
 
+/*
+
+What is Upper Bound?
+
+- Returns an index of element which is NEXT GREATER than the target element in the array
+- If not found, return length of the array (length + 1, as we want index)
+
+Ex. [1, 3, 3, 4, 5, 6, 9], target = 3
+Answer = 3 (element 4)
+ 
+*/
+
 class UpperBound {
+    
+    public static int upperBound(int[] arr, int l, int r, int target) {
+        
+        while(l < r) {
+            
+            int mid = l + ((r-l)/2);
+            
+            if(arr[mid] > target)
+                r = mid;
+            
+            else 
+                l = mid+1;
+        }
+        
+        return l;
+    }
+    
     public static void main(String[] args) {
-        var arr = new int[] {1,2,3,4,5,5,5,6,7,9,9};
+        var arr = new int[] {5,7,7,8,8,10};
         
         int n = arr.length;
 
-        int upper_bound = Common.upperBound(arr, 0, n-1, 5);
+        int upper_bound = upperBound(arr, 0, n-1, 8);
 
         System.out.println(upper_bound);
     }
---
+++ src/binary_search/5_Search_In_Rotated_Sorted_Array.java
@@ -0,0 +1,9 @@
+package binary_search;
+
+class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        var arr = new int[] {4,5,6,7,0,1,2};
+        
+        
+    }
+}
